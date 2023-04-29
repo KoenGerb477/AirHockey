0c089c7ca9ae46cb942c6d668df90444168c504e
Koen Gerber <koengerb477@ed.amdsb.ca>
2023-04-29 16:35:52 -0400

    Add project files.

---
+++ AirHockey/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace AirHockey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.AutoSize = true;
+            this.player1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player1ScoreLabel.Font = new System.Drawing.Font("Cascadia Code", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(12, 297);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(50, 57);
+            this.player1ScoreLabel.TabIndex = 0;
+            this.player1ScoreLabel.Text = "0";
+            // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.AutoSize = true;
+            this.player2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player2ScoreLabel.Font = new System.Drawing.Font("Cascadia Code", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(12, 562);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(50, 57);
+            this.player2ScoreLabel.TabIndex = 1;
+            this.player2ScoreLabel.Text = "0";
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Cascadia Code", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.White;
+            this.winLabel.Location = new System.Drawing.Point(81, 115);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(0, 57);
+            this.winLabel.TabIndex = 2;
+            this.winLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 900);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label winLabel;
+    }
+}
+
---
+++ AirHockey/Form1.cs
@@ -0,0 +1,343 @@
+﻿/*
+ * 
+ * Koen Gerber
+ * ICS3U
+ * Mr T
+ * April 29, 2023
+ * 
+ * Air Hockey Summative
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+
+namespace AirHockey
+{
+    public partial class Form1 : Form
+    {
+        Rectangle puck = new Rectangle(280, 430, 40, 40);
+        Rectangle border = new Rectangle(5, 5, 590, 890);
+        Rectangle player1 = new Rectangle(275, 200, 50, 50);
+        Rectangle player2 = new Rectangle(275, 650, 50, 50);
+
+        SolidBrush whiteBrush = new SolidBrush (Color.White);
+        SolidBrush blueBrush = new SolidBrush (Color.Blue);
+        SolidBrush greenBrush = new SolidBrush(Color.LimeGreen);
+        Pen redPen = new Pen(Color.Red, 10);
+        Pen bluePen = new Pen(Color.Blue, 10);
+
+        SoundPlayer augh = new SoundPlayer(Properties.Resources.aughSound);
+        SoundPlayer boing = new SoundPlayer(Properties.Resources.boingSound);
+        SoundPlayer hooray = new SoundPlayer(Properties.Resources.hooraySound);
+        SoundPlayer score = new SoundPlayer(Properties.Resources.scoreSound);
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        int playerSpeed = 10;
+        int puckXSpeed = 0;
+        int puckYSpeed = 0;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftDown = false;
+        bool rightDown = false;
+
+
+        int tickCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //draw screen
+            e.Graphics.DrawArc(bluePen, 175, 325, 250, 250, 0, 360);
+            e.Graphics.DrawLine(bluePen, 0, 450, 600, 450);
+            e.Graphics.DrawRectangle (redPen, border);
+            e.Graphics.DrawLine(bluePen, 200, 5, 400, 5);
+            e.Graphics.DrawLine(bluePen, 200, 895, 400, 895);
+            e.Graphics.FillPie(blueBrush, 200, -90, 200, 200, 0, 180);
+            e.Graphics.FillPie(blueBrush, 200, 810, 200, 200, 180, 360);
+            e.Graphics.FillRectangle(greenBrush, player1);
+            e.Graphics.FillRectangle(greenBrush, player2);
+            e.Graphics.FillEllipse(whiteBrush, puck);
+
+            //update score
+            player1ScoreLabel.Text = $"{player1Score}";
+            player2ScoreLabel.Text = $"{player2Score}";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move puck
+            puck.Y += puckYSpeed;
+            puck.X += puckXSpeed;
+
+            //move player 1 
+            if (wDown == true && player1.Y > 10)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sDown == true && player1.Y < 445 - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            if (aDown == true && player1.X > 10)
+            {
+                player1.X -= playerSpeed;
+            }
+
+            if (dDown == true && player1.X < this.Width - player1.Width - 10)
+            {
+                player1.X += playerSpeed;
+            }
+
+            //move player 2 
+            if (upArrowDown == true && player2.Y > 455)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height - 10)
+            {
+                player2.Y += playerSpeed;
+            }
+
+            if (leftDown == true && player2.X > 10)
+            {
+                player2.X -= playerSpeed;
+            }
+
+            if (rightDown == true && player2.X < this.Width - player2.Width - 10)
+            {
+                player2.X += playerSpeed;
+            }
+
+            //check if puck hit top or bottom wall and change direction if it does and check if it goes in goal
+            if (puck.Y < 10)
+            {
+                puck.Y = 10;
+                puckYSpeed = -puckYSpeed;
+
+               if (puck.X >= 200 && puck.X + puck.Width <= 400)
+                {
+                    score.Play();
+                    
+                    player2Score++;
+
+                    puck.X = 280;
+                    puck.Y = 370;
+
+                    ResetPositions();
+                }
+            }
+
+            if (puck.Y > this.Height - puck.Height - 10)
+            {
+                puck.Y = this.Height - puck.Height - 10;
+                puckYSpeed = -puckYSpeed;
+
+                if (puck.X >= 200 && puck.X + puck.Width <= 400)
+                {
+                    score.Play();
+
+                    player1Score++;
+
+                    puck.X = 280;
+                    puck.Y = 490;
+                    
+                    ResetPositions();
+                }
+            }
+
+            //check if puck hit right wall or left wall and change direction
+            if (puck.X + puck.Width > 590)
+            {
+                puck.X = 590 - puck.Width;
+                puckXSpeed *= -1;
+            }
+            if (puck.X < 10)
+            {
+                puck.X = 10;
+                puckXSpeed *= -1;
+            }
+
+            //check if puck hits either player. If it does change the direction 
+            //and place the puck in front of the player hit 
+            if (player1.IntersectsWith(puck))
+            {
+                augh.Play();
+
+                int x = player1.X;
+                int y = player1.Y;
+                CheckCollisionDirection(x, y);
+            }
+            if (player2.IntersectsWith(puck))
+            {
+                boing.Play();
+                
+                int x = player2.X;
+                int y = player2.Y;
+                CheckCollisionDirection(x, y);
+            }
+            
+            //slow puck down every ten ticks
+            if (tickCounter % 10 == 0)
+            {
+                //slow down puck
+                if (puckXSpeed > 0)
+                {
+                    puckXSpeed -= 1;
+                }
+                if (puckYSpeed > 0)
+                {
+                    puckYSpeed -= 1;
+                }
+            }
+            tickCounter++;
+
+            //check for winners
+            if (player1Score == 3)
+            {
+                hooray.Play();
+                
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Player  1  Wins!!!";
+            }
+            else if (player2Score == 3)
+            {
+
+                hooray.Play();
+                gameTimer.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Player  2  Wins!!!";
+            }
+
+            Refresh();
+
+        }
+        
+        public void ResetPositions()
+        {
+            puckXSpeed = 0;
+            puckYSpeed = 0;
+
+            player1.Y = 200;
+            player1.X = 275;
+            player2.Y = 650;
+            player2.X = 275;
+        }
+
+        public void CheckCollisionDirection(int playerX, int playerY)
+        {
+
+            //puck to left of paddle
+            if (puck.X <= playerX && puck.Y <= playerY + player1.Height / 2 && puck.Y >= playerY - player1.Height / 2)
+            {
+                puck.X -= puck.Width;
+                puckXSpeed = -10;
+            }
+            //puck to top of paddle
+            if (puck.Y <= playerY && puck.X <= playerX + player1.Width / 2 && puck.X >= playerX - player1.Width / 2)
+            {
+                puck.Y -= puck.Height;
+                puckYSpeed = -10;
+            }
+            //puck to right of paddle
+            if (puck.X >= playerX && puck.Y <= playerY + player1.Height / 2 && puck.Y >= playerY - player1.Height / 2)
+            {
+                puck.X -= puck.Width;
+                puckXSpeed = 10;
+            }
+            //puck to bottom of paddle
+            if (puck.Y >= playerY && puck.X <= playerX + player1.Width / 2 && puck.X >= playerX - player1.Width / 2)
+            {
+                puck.Y += puck.Height;
+                puckYSpeed = 10;
+            }
+        }
+    }
+}
